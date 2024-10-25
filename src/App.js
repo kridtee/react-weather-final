bbf9c6715b74b94f06b2751d63e853812f64763a
kridtee <60269474+kridtee@users.noreply.github.com>
2024-10-25 08:31:06 -0400

    Added GitHub URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">React Weather App</div>;
+  return (
+    <div className="App">
+      React Weather App
+      <footer>
+        This project was coded by Kirsty H and is{" "}
+        <a href="https://snazzy-taiyaki-fc5a32.netlify.app" target="_blank">
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
