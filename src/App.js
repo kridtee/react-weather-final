7b46bdd6fb3d79576454cbf4d3af05af2ae77183
kridtee <60269474+kridtee@users.noreply.github.com>
2024-10-25 08:56:54 -0400

    Added bootstrap NPM

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      React Weather App
-      <footer>
-        This project was coded by Kirsty H and is{" "}
-        <a href="https://snazzy-taiyaki-fc5a32.netlify.app" target="_blank">
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>React Weather App</h1>
+        <button className="btn btn-primary">Hello</button>
+        <footer>
+          This project was coded by Kirsty H and is{" "}
+          <a href="https://snazzy-taiyaki-fc5a32.netlify.app" target="_blank">
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
