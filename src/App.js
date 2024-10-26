d12cf697a9dca8c99afeccfac2eb151bdffce8c2
kridtee <60269474+kridtee@users.noreply.github.com>
2024-10-26 14:56:19 -0400

    Added Weather Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>React Weather App</h1>
+        <Weather />
         <button className="btn btn-primary">Hello</button>
         <footer>
           This project was coded by Kirsty H and is{" "}
