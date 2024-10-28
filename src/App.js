16439dcefb86e57227d2a60d6d6c1f8e0802af09
kridtee <60269474+kridtee@users.noreply.github.com>
2024-10-28 09:29:45 -0400

    API integrations & Date

--- src/App.js
+++ src/App.js
@@ -5,11 +5,15 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather />
+        <Weather defaultCity="New York" />
         <button className="btn btn-primary">Hello</button>
         <footer>
           This project was coded by Kirsty H and is{" "}
-          <a href="https://snazzy-taiyaki-fc5a32.netlify.app" target="_blank">
+          <a
+            href="https://snazzy-taiyaki-fc5a32.netlify.app"
+            target="_blank"
+            rel="noreferrer"
+          >
             open-sourced on GitHub
           </a>
         </footer>
